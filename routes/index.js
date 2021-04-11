73cea065cdac885f8aeaac8137624211cce32144
HarshaVardhanNakkina <harshavardhan.n@yahoo.com>
2021-04-11 15:57:18 +0530

    assignment7 CRUD employee - CRU done, D remaining

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+
+const pool = require('../db')
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-	res.render('home.html', { name: 'Harsha', title: 'MySQL Assignment' })
+router.get('/', async function (req, res, next) {
+	try {
+		const [results, fields] = await pool.execute(
+			`
+            SELECT
+                e.empid 'id', e.ename 'name', e.job 'job', e.sal 'salary', d.dname 'department'
+            FROM
+                employee e NATURAL JOIN dept d
+            `
+		)
+
+		res.render('employees/index.html', { results, title: 'Employees List' })
+	} catch (error) {
+		next(error)
+	}
 })
 
 module.exports = router
